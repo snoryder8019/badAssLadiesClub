a7dfb42d64a32d4e988f77cd364cc069c576dd17
snoryder8019 <snoryder8019@gmail.com>
2022-02-11 13:41:12 -0700

    removed ba branding, coreected language,

--- public/jsFiles/blcMenu.js
+++ public/jsFiles/blcMenu.js
@@ -4,7 +4,7 @@ const blcMenu0 = document.getElementById('blcMenu0');
 const blcMenu1 = document.getElementById('blcMenu1');
 const blcMenu2 = document.getElementById('blcMenu2');
 const blcMenu3 = document.getElementById('blcMenu3');
-const blcMenu5 = document.getElementById('blcMenu5');
+
 
 function menuPageGet(zzx){
 	zzx.style.display="block";

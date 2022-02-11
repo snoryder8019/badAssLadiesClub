6bdfac494a018235bc95f9cc29a3e7d54831b47e
snoryder8019 <snoryder8019@gmail.com>
2022-02-11 12:19:26 -0700

    d

--- routes/links.js
+++ routes/links.js
@@ -5,5 +5,7 @@ router.get('/feelGood',(req,res)=>{
 res.redirect('https://badassladiesclub-school.thinkific.com/courses/feelinggood')});
 router.get('/w2',(req,res)=>{
 res.redirect('http://www.w2marketing.biz')});
+router.get('/purple',(req,res)=>{
+    res.redirect('http://www.purplereignftw.com')});
 
 module.exports = router;

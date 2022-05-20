f9fee6a0b5dff5f89529bfa4add152f0672e72a9
snoryder8019 <snoryder8019@gmail.com>
2022-05-20 12:19:47 -0600

    Updated menus, added podcasts

--- routes/links.js
+++ routes/links.js
@@ -7,5 +7,6 @@ router.get('/w2',(req,res)=>{
 res.redirect('http://www.w2marketing.biz')});
 router.get('/purple',(req,res)=>{
     res.redirect('http://www.purplereignftw.com')});
-
+    router.get('/lawrieWork',(req,res)=>{
+        res.redirect('https://calendly.com/lawriewallace/30min?month=2022-05')});
 module.exports = router;
